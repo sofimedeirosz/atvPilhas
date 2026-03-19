3e4aaba7f09e1c677c92b4875a229c85ec26702b
Sofia Medeiros <sofia.medeiros@edu.unifil.br>
2026-03-19 19:47:42 -0300

    Add files via upload

---
+++ MainPilhaSimples.java
@@ -0,0 +1,15 @@
+public class MainPilhaSimples {
+    public static void main(String[] args) {
+        PilhaSimples pilhaSimples = new PilhaSimples(6);
+        pilhaSimples.empilhar("Ricardo");
+        pilhaSimples.empilhar("Tania");
+        pilhaSimples.empilhar("Marcelo");
+        pilhaSimples.empilhar("Igor");
+        pilhaSimples.empilhar("João");
+        pilhaSimples.empilhar("Robson");
+        pilhaSimples.empilhar("Bruna");
+        pilhaSimples.exibirPilha();
+        pilhaSimples.desempilhar();
+        pilhaSimples.exibirPilha();
+    }
+}
